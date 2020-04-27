92bcc286aafedba22cd2cb4d703a451afd099f62
Nyameaama Gambrah <nyameaama@Nyameaamas-MacBook-Pro.local>
2020-04-27 14:44:22 +0000

    improvements

---
+++ src/Format/formatData.h
@@ -0,0 +1,13 @@
+#ifndef FORMAT
+#define FORMAT
+
+#include<string>
+
+class formatData {
+    public:
+        formatData(std::string x);
+
+
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include"IO/writeOut.h"
 #include"IO/read_in.h"
 #include"Pref/getOS.h"
+#include<iostream>
 
 int main(){
+    //Declare objects
+    writeOut get;
+    Terminal_read tx;
+    //Get block of text
+    tx.BeginTextPrompt();
+    std::string Ttext = tx.getTerminalText();
+    system("clear");
+    std::cout << "Processing";
+    get.fileWrite(Ttext);
+    get.file_Read();
+    system("clear");
+    
 
 }
